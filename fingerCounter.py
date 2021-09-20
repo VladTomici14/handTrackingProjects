b97439581f580151943d499de3b2bf6e7b0f32b5
VladTomici14 <vladtomici@gmail.com>
2021-09-20 09:55:28 +0300

    added the fingerCounter.py file

--- fingerCounter.py
+++ fingerCounter.py
@@ -2,49 +2,53 @@ from handTrackingModule import handsDetector
 import time
 import cv2
 
-# camera variables
-camera = cv2.VideoCapture(0)
-ret, testFrame = camera.read()
-(height, width) = testFrame.shape[:2]
-
-# time variables
-currentTime = 0
-previousTime = 0
-
-handdetector = handsDetector(staticImageModel = False,
-                             maxNumHands = 1,
-                             minDetectionConfidence = 0.5,
-                             minTrackingConfidence = 0.5)
-
-def verifyFinger():
-
-    return True
-
 def main():
+    # camera variables
+    camera = cv2.VideoCapture(0)
+    t, testFrame = camera.read()
+    (height, width) = testFrame.shape[:2]
+
+    # hand variables
+    handdetector = handsDetector(staticImageModel = False,
+                                 maxNumHands = 1,
+                                 minDetectionConfidence = 0.5,
+                                 minTrackingConfidence = 0.5)
+
+    # time variables
+    currenTime = 0
+    previousTime = 0
+
     while True:
-        t, frame = camera.read()
+        ret, frame = camera.read()
 
-        handlandmarks = handdetector.findHands(frame, draw = True)
+        frame = handdetector.findHands(frame = frame, draw = False)
+        landmarksList = handdetector.findPosition(frame, draw = False)
 
-        if len(handlandmarks) != 0:
+        if len(landmarksList) != 0:
             numberOfFingers = 0
-            for i in range(6):
-                fingerNumber = (i + 1) * 4
-                if verifyFinger(fingerNumber) == True:
-                    numberOfFingers += 1
-            print(f"number of fingers: {int(numberOfFingers)}")
+            for i in range(5):
+                fingerTip = (i+1) * 4
+                if i == 0:
+                    fingerMid = 5
+                else:
+                    fingerMid = fingerTip - 2
 
-        # calculating the FPS
+                yCoord = [coord[1] for coord in landmarksList]
+                if yCoord[fingerTip] < yCoord[fingerMid]:
+                    numberOfFingers += 1
+            print(numberOfFingers)
+            cv2.putText(frame, f"Number of fingers: {str(int(numberOfFingers))}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+        # calculatint FPS
         currentTime = time.time()
         fps = 1 / (currentTime - previousTime)
         previousTime = currentTime
 
+        cv2.putText(frame, f"FPS: {str(int(fps))}", (width // 2 + 100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+        cv2.imshow("frame", frame)
+
         if cv2.waitKey(1) == ord("q"):
             break
-
-        camera.release()
-        cv2.destroyAllWindows()
-
 
 if __name__ == "__main__":
     main()
